c583e730c0289b60ee49f3f53ecd1999e439bbb0
betanyeli <betangelii@gmail.com>
2019-05-28 21:06:19 -0400

    borderplate listo

---
+++ src/assets/js/auth.js
@@ -0,0 +1,5 @@
+//Autenticación
+// adentro de las llaves va el nombre de la funcion q quiero importar.
+/*import { logoutMessage, user } from './logout.js'
+logoutMessage();
+console.log(user);*/
---
+++ src/assets/js/logout.js
@@ -0,0 +1,9 @@
+/*
+export const logoutMessage = () => {
+    console.log("Goodbye");
+} 
+
+export const user = {
+    name: "beta",
+    job: "student"
+}*/
