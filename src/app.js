552f3c5045c56d1aab4fb1eec85b953492f90bd7
betanyeli <betangelii@gmail.com>
2019-05-31 13:53:16 -0400

    primer estilo de login, sin separar archivos

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-//Global de firebase
 const firebaseConfig = {
     apiKey: "AIzaSyA7ctYRP7x6WBe_PwZCUnYYe2ahkL7b6l4",
     authDomain: "nomorecommunity-2cfdd.firebaseapp.com",
@@ -10,58 +9,44 @@ const firebaseConfig = {
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
-
-import { registerUser } from './assets/js/auth'
-import { signWithMail } from './assets/js/auth'
-import { logout } from './assets/js/auth'
-
-//let result="";
-
-  /*Btn Register*/
 let btnRegister= document.getElementById("register");
     btnRegister.addEventListener("click", () => {
         let emailOne = document.getElementById("email").value;
         let passwordOne = document.getElementById("password").value;
-       registerUser(emailOne, passwordOne)
-        /*firebase.auth().createUserWithEmailAndPassword(emailOne, passwordOne).catch(function(error) {
+        firebase.auth().createUserWithEmailAndPassword(emailOne, passwordOne).catch(function(error) {
             // Handle Errors here.
             let errorCode = error.code;
             let errorMessage = error.message;
             console.log(errorCode)
             console.log(errorMessage)
            
-          });     */
-         
-        })
-  
+          });     
+  })
 
-  /*Btn Sign In*/
   let btnSignIn= document.getElementById("sign-in");
     btnSignIn.addEventListener("click", () => {
         let emailTwo = document.getElementById("email2").value;
         let passwordTwo = document.getElementById("password2").value;
-       signWithMail(emailTwo, passwordTwo);
-        /*firebase.auth().signInWithEmailAndPassword(emailTwo, passwordTwo).catch(function(error) {
+        firebase.auth().signInWithEmailAndPassword(emailTwo, passwordTwo).catch(function(error) {
             // Handle Errors here.
             let errorCode = error.code;
             let errorMessage = error.message;
             console.log(errorCode)                                                                                                                                                          
             console.log(errorMessage)
             // ...
-          });*/
-         
+          });
+           
           });  
-
-    /*Observador de inicio de sesión*/
 function observer() {
     firebase.auth().onAuthStateChanged(function(user) {
         if (user) {
-         
-            //alert("existe este user")
+          show()
+            console.log("existe este user")
           // User is signed in.
           let displayName = user.displayName;
           let email = user.email;
           let emailVerified = user.emailVerified;
+          console.log(emailVerified)
           let photoURL = user.photoURL;
           let isAnonymous = user.isAnonymous;
           let uid = user.uid;
@@ -69,20 +54,25 @@ function observer() {
           // ...
         } else {
           // User is signed out.
-         //alert("NO existe este user")
+          console.log("NO existe este user")
         }
       });
 }
 observer();
 
-/*Btn Logout*/
+//Logout
 let btnSignOut = document.getElementById("sign-out");
 btnSignOut.addEventListener("Click", () => {
-    logout();
-  /*firebase.auth().signOut().then(function() {
+  firebase.auth().signOut().then(function() {
   // Sign-out successful.
 }).catch(function(error) {
   // An error happened.
-});*/
-
+});
 })
+
+function show(){
+  let content= document.getElementById("testing");
+  content.innerHTML= "Bienvenido, esto es un test!!!!!"
+}
+
+///////////// testing
--- src/assets/js/auth.js
+++ src/assets/js/auth.js
@@ -1,36 +1,108 @@
 /*Todas las funciones que autoricen el inicio/cierre de sesión*/
-
-
-
+/*
 //Registro de usuario
   export const registerUser = (email, password) => {
       
-    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-        // Handle Errors here.
-        let errorCode = error.code;
-        let errorMessage = error.message;
-        console.log(errorCode)
-        console.log(errorMessage)
-       
-      }); 
+    return firebase.auth().createUserWithEmailAndPassword(email, password);
   }
       
 //Inicio de sesión con correo electrónico y contraseña
   export const signWithMail = (email, password) => {
-    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-        // Handle Errors here.
-        let errorCode = error.code;
-        let errorMessage = error.message;
-        console.log(errorCode)                                                                                                                                                          
-        console.log(errorMessage)
-        // ...
-      });
+    return firebase.auth().signInWithEmailAndPassword(email, password);
        
   }
 
   //Cierre de sesión
-  export const logout = firebase.auth().signOut().then(function() {
-    // Sign-out successful.
-  }).catch(function(error) {
-    // An error happened.
-  });
+  export const logout = () => {
+return firebase.auth().signOut()
+  } 
+*/
+//Global de firebase
+/*const firebaseConfig = {
+    apiKey: "AIzaSyA7ctYRP7x6WBe_PwZCUnYYe2ahkL7b6l4",
+    authDomain: "nomorecommunity-2cfdd.firebaseapp.com",
+    databaseURL: "https://nomorecommunity-2cfdd.firebaseio.com",
+    projectId: "nomorecommunity-2cfdd",
+    storageBucket: "nomorecommunity-2cfdd.appspot.com",
+    messagingSenderId: "108376578479",
+    appId: "1:108376578479:web:da6682d105a33503"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+/*import { registerUser } from './assets/js/auth'
+import { signWithMail } from './assets/js/auth'
+import { logout } from './assets/js/auth'*/
+
+//let result="";
+
+  /*Btn Register*/
+/*let btnRegister= document.getElementById("register");
+    btnRegister.addEventListener("click", () => {
+        let emailOne = document.getElementById("email").value;
+        let passwordOne = document.getElementById("password").value;
+       registerUser(emailOne, passwordOne)
+    firebase.auth().createUserWithEmailAndPassword(emailOne, passwordOne).catch(function(error) {
+            // Handle Errors here.
+            let errorCode = error.code;
+            let errorMessage = error.message;
+            console.log(errorCode)
+            console.log(errorMessage)
+           
+          });     
+         
+        })
+  
+
+  /*Btn Sign In*/
+  /*let btnSignIn= document.getElementById("sign-in");
+    btnSignIn.addEventListener("click", () => {
+        let emailTwo = document.getElementById("email2").value;
+        let passwordTwo = document.getElementById("password2").value;
+       signWithMail(emailTwo, passwordTwo);
+    firebase.auth().signInWithEmailAndPassword(emailTwo, passwordTwo).catch(function(error) {
+            // Handle Errors here.
+            let errorCode = error.code;
+            let errorMessage = error.message;
+            console.log(errorCode)                                                                                                                                                          
+            console.log(errorMessage)
+            // ...
+          });
+         
+          });  
+
+    /*Observador de inicio de sesión*/
+/*function observer() {
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+         
+            //alert("existe este user")
+          // User is signed in.
+          let displayName = user.displayName;
+          let email = user.email;
+          let emailVerified = user.emailVerified;
+          let photoURL = user.photoURL;
+          let isAnonymous = user.isAnonymous;
+          let uid = user.uid;
+          let providerData = user.providerData;
+          // ...
+        } else {
+          // User is signed out.
+         //alert("NO existe este user")
+        }
+      });
+}
+observer();
+
+
+/*Btn Logout*/
+/*let btnSignOut = document.getElementById("sign-out");
+btnSignOut.addEventListener("Click", () => {
+    logout();
+  firebase.auth().signOut().then(function() {
+  // Sign-out successful.
+}).catch(function(error) {
+  // An error happened.
+});
+
+})*/
