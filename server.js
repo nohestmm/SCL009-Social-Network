8374b63ec9e9216b63641a39f8c66c9c4716820c
bigudam <dharma.herrera@gmail.com>
2019-06-11 12:18:54 -0400

    conflictos en eslintrc, gitignore, package-json,server

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ app.get('/',function(request, response){
   response.sendFile(__dirname + '/index.html');
 });
 
-app.listen(8800, function(){
+app.listen(4200, function(){
   console.log('Server Express Ready!');
 });
