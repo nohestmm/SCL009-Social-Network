07060b510697f5e311ddd3e9d0d2cdf2f59c53f1
nohestmm <nohestmm@gmail.com>
2019-06-12 16:46:56 -0400

    subiendo datos del popup de correo de google

--- src/assets/js/firebaseAuth.js
+++ src/assets/js/firebaseAuth.js
@@ -2,13 +2,13 @@ import { templateLogin, userInvalid, emailVerify } from "../views/templateLogin.
 //export let name, email;
 
 // Guardar usuarios registrados en firestore
-const saveUsers = (name, email, password, uid) => {
+const saveUsers = (name, email,uid) => {
   let db = firebase.firestore();
   db.collection("users").add({
     uid: uid,
     name: name,
-    email: email,
-    password: password
+    email: email
+   
 
   })
     .then(function (docRef) {
@@ -31,7 +31,7 @@ export const register = (name, email, password) => {
       console.log(uid);
       verifyAccount();
       observer();
-      saveUsers(name, email, password, uid);
+      saveUsers(name, email,uid);
      
       window.location.hash = '#/login';
 
@@ -117,9 +117,15 @@ export const googleAuth = () => {
     var token = result.credential.accessToken;
     console.log(token);
     // The signed-in user info.
-    var user = result.user;
+   let user = result.user;
     console.log(user);
+let name = user.displayName;
+console.log(name);
+let email = user.email;
+console.log(email);
+let uid = user.uid;
 
+saveUsers(name, email, uid);
     window.location.hash = '#/project';
     // ...
   }).catch(error => {
